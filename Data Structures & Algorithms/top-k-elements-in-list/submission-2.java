048b0536932dd4904e0584ca90019a25125c6231
Disha Singh <1ms24ai019@msrit.edu>
2026-09-17 08:43:15 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        int n = nums.length;
+        int[] freq = new int[n];
+        for(int i =0; i < n ; i++){
+            if(freq[i] != -1){
+                freq[i] = 1;
+
+            for(int j = i+1; j < n ; j++){
+                if(nums[i] == nums[j]){
+                    freq[i]++;
+                    freq[j] = -1;
+                }
+            }
+            }
+        }
+        int[] result = new int[k];
+        for(int x = 0; x < k ; x++){
+            int max = -1;
+            int index = -1;
+            for(int i =0; i < n ; i++){
+            if(freq[i] > max){
+                max = freq[i];
+                index = i;
+            }
+            }
+            result[x] = nums[index];
+            freq[index] = -1;
+        }
+
+        return result;
+    }
+}
